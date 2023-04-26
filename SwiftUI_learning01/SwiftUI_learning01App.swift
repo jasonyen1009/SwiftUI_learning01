5391b3cd43d09476aa18d85297bacbc1e5ad63b3
Jason <jasonyen1009@gmail.com>
2023-04-26 13:52:18 +0800

    Initial Commit

---
+++ SwiftUI_learning01/SwiftUI_learning01App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_learning01App.swift
+//  SwiftUI_learning01
+//
+//  Created by Yen Hung Cheng on 2023/4/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_learning01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
